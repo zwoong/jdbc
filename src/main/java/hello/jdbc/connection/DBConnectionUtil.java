819979214f729fc5c7de9cf65ac382934320d989
zwoong <zwoong6867@gmail.com>
2025-05-26 12:39:29 +0900

    ✨ 데이터베이스 연결

---
+++ src/main/java/hello/jdbc/connection/DBConnectionUtil.java
@@ -0,0 +1,23 @@
+package hello.jdbc.connection;
+
+import static hello.jdbc.connection.ConnectionConst.*;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+public class DBConnectionUtil {
+  public static Connection getConnection() {
+
+    try {
+      Connection connection = DriverManager.getConnection(URL, USERNAME, PASSWORD);
+      log.info("get connection={}, class={}", connection, connection.getClass());
+      return connection;
+    } catch (Exception e) {
+      log.error(e.toString());
+    }
+
+    return null;
+  }
+}
